86c0c46f863838e60366709466a43129c5a04be6
Joel Salisbury <joel.salisbury@gmail.com>
2024-08-16 12:07:21 -0400

    now cards, containing all pertinent info

--- directory/resources/views/institutes/index.blade.php
+++ directory/resources/views/institutes/index.blade.php
@@ -5,25 +5,43 @@
 @section('content')
 
 <div class="row">
-    <h1>Institutes by State and IHE</h1>
+    <div class="col-md-12"><h1>Institutes by State and IHE</h1>
+    </div>
+</div>
 
     @foreach($institutes as $state => $stateGroup)
-        <h2>{{ $state }}</h2>
-
+        <div class="state row">
+            <div class="col-md-12"><h2>{{ $state }}</h2></div>
+        </div>
         @foreach($stateGroup as $ihe => $programs)
-            <h3>{{ $ihe }}</h3>
+            <div class="ihe row my-3">
+                <div class="col-md-12">
+                    <h3>{{ $ihe }}</h3>
+                </div>
 
-            <ul>
                 @foreach($programs as $program)
-                    <li>
-                        <strong>{{ $program->program_title }}</strong> ({{ $program->level_of_degree }})<br>
-                        Format: {{ $program->format }}<br>
-                        Alternate Route: {{ $program->alternate_route_to_certification ? 'Yes' : 'No' }}<br>
-                        Category: {{ $program->category_of_credentialing }}<br>
-                        <a href="{{ $program->url_for_program }}" target="_blank">Program Link</a>
-                    </li>
+                    <div class="program col-md-4">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">{{ $program->program_title }}</h5>
+                            </div>
+                            <div class="card-body">
+
+                                <h6 class="card-subtitle mb-2 text-muted">{{ $program->level_of_degree }}</h6>
+                                <div class="card-text">
+                                    <p>{{ $program->format }}</p>
+                                    <p><strong>Category of Credentialing:</strong> {{ $program->category_of_credentialing }}</p>
+                                    <p><strong>Offers Alternate Route to Certificiation:</strong> {{ $program->alternate_route_to_certification ? 'Yes' : 'No' }}</p>
+                                </div>
+
+                            </div>
+                            <div class="card-footer">
+                                <strong>Program Website:</strong> <a href="{{ $program->program_url }}" target="_blank">Visit Website</a>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
-            </ul>
+            </div>
         @endforeach
     @endforeach
 </div>
